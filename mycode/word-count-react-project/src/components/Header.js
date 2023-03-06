bab4e0101b7f257e9ed506c468407c438edb26a6
Adriansyah Ravindra <adrimedia.indonesia@gmail.com>
2023-03-06 13:28:49 +0700

    Word Counter Project & React Testing/Jest Exercise.
    Commit membuat Word Counter Project & belajar React Testing/Jest Exercise.

---
+++ mycode/word-count-react-project/src/components/Header.js
@@ -0,0 +1,15 @@
+export const Header = ({heading, subHeading}) => {
+
+    return (
+
+      <section className="header">
+
+          <h1 data-testid="heading">{heading}</h1>
+
+          <p data-testid="subHeading">{subHeading}</p>
+
+      </section>
+
+    )
+
+  }
